96c0bc195d0591af16e7ad2c9d90b0efb1a8eb85
Cameron Zaas <cameronzaas@gmail.com>
2019-04-10 19:46:47 -0700

    - added images - added dependencies - commented out form

--- gulpfile.js
+++ gulpfile.js
@@ -1,111 +1,81 @@
-var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	autoPrefix = require('gulp-autoprefixer'),
-	watch = require('gulp-watch'),
-	bs = require('browser-sync'),
+var gulp = require("gulp"),
+	sass = require("gulp-sass"),
+	autoPrefix = require("gulp-autoprefixer"),
+	watch = require("gulp-watch"),
+	bs = require("browser-sync"),
 	reload = bs.reload,
-	minifyCss = require('gulp-minify-css'),
-	jsHint = require('gulp-jshint'),
-	jsUglify = require('gulp-uglify'),
-	concat = require('gulp-concat'),
-	minifyHtml = require('gulp-minify-html'),
-	lwip = require('lwip'),
-	fs = require('fs-extra');
+	minifyCss = require("gulp-minify-css"),
+	jsUglify = require("gulp-uglify"),
+	concat = require("gulp-concat"),
+	minifyHtml = require("gulp-minify-html"),
+	fs = require("fs-extra");
 
-gulp.task('reload', function(){
+gulp.task("reload", function() {
 	bs.init({
-		server: './'
+		server: "./",
 	});
 
-	gulp.watch('./css/*.sass', ['css']);
-	gulp.watch('./js/*.js', ['js']);
-	gulp.watch('./html/*.html', ['html']);
+	gulp.watch("./css/*.sass", ["css"]);
+	gulp.watch("./js/*.js", ["js"]);
+	gulp.watch("./html/*.html", ["html"]);
 });
 
-gulp.task('html', function(){
-	
+gulp.task("html", function() {
 	var opts = {
 		conditionals: true,
 		empty: true,
 	};
-	
-	gulp.src('./html/*.html')
-	.pipe(minifyHtml(opts))
-	.pipe(gulp.dest('./'))
-	.pipe(reload({stream: true}));
+
+	gulp.src("./html/*.html")
+		.pipe(minifyHtml(opts))
+		.pipe(gulp.dest("./"))
+		.pipe(reload({ stream: true }));
 });
 
-gulp.task('css', function(){
-	gulp.src('./css/*.sass')
+gulp.task("css", function() {
+	gulp.src("./css/*.sass")
 		.pipe(sass({ indentedSyntax: true }))
 		.pipe(autoPrefix())
 		.pipe(minifyCss())
-		.pipe(gulp.dest('./css/'))
-		.pipe(reload({stream: true}));
+		.pipe(gulp.dest("./css/"))
+		.pipe(reload({ stream: true }));
 });
 
-gulp.task('js', function(){
-	gulp.src(['./js/testimonials.js','./js/shuffle.js','./js/main.js','./js/listeners.js'])
-		.pipe(jsHint())
-		.pipe(jsHint.reporter('jshint-stylish'))
-		.pipe(concat('concat.js'))
+gulp.task("js", function() {
+	gulp.src([
+		"./js/testimonials.js",
+		"./js/shuffle.js",
+		"./js/main.js",
+		"./js/listeners.js",
+	])
+
+		.pipe(concat("concat.js"))
 		.pipe(jsUglify())
-		.pipe(gulp.dest('./js'))
-		.pipe(reload({stream: true}));
+		.pipe(gulp.dest("./js"))
+		.pipe(reload({ stream: true }));
 });
 
-gulp.task('minify-images', function(){
+gulp.task("rename-images", function() {
+	fs.readdir("./_under_contruction/images/", function(err, files) {
+		if (err) {
+			console.error(err);
+		}
 
-	//need to fs.readdir and for loop over all the images and run them through the minifyImage function
-	
-	fs.readdir('./images/slideshow/', function(err, files){
-		if(err){ console.error(err); }
-		
-		var currentDir = 'images/slideshow/';
-		var destDir = 'images/min/';
-		
-		files.forEach(function(file, i, arr){
-			minifyImage(file, currentDir, destDir);
-		});
-	});
-	
-	//minifyImage('image-0.jpg', 'images/slideshow/', 'test/');
-	
-	function minifyImage(img, src, dest){
-		lwip.open(src + img, function(err, image){
-		
-		// check err...
-		// define a batch of manipulations and save to disk as JPEG:
-		image.batch()
-			.scale(0.25)          // scale to 25%
-			//.rotate(45, 'white')  // rotate 45degs clockwise (white fill)
-			//.crop(200, 200)       // crop a 200X200 square from center
-			//.blur(5)              // Gaussian blur with SD=5
-			.writeFile(dest + img, function(err){
-				if(err) { console.error(err); }
-				
-				console.log('finished');
-			// check err...
-			// done.
-			});
-		});
-	}
-});
+		var currentDir = "_under_contruction/images/";
+		var destDir = "images/min/";
 
-gulp.task('rename-images', function(){
-	fs.readdir('./_under_contruction/images/', function(err, files){
-		if(err){ console.error(err); }
-		
-		var currentDir = '_under_contruction/images/';
-		var destDir = 'images/min/';
-		
-		files.forEach(function(file, i, arr){
-			fs.copy(currentDir + file, destDir + 'image-' + i + '.jpg', function(err){
-				if(err) { console.error(err); } 
-			}); 
+		files.forEach(function(file, i, arr) {
+			fs.copy(
+				currentDir + file,
+				destDir + "image-" + i + ".jpg",
+				function(err) {
+					if (err) {
+						console.error(err);
+					}
+				}
+			);
 		});
 	});
 });
 
-
-gulp.task('default', ['reload']);
+gulp.task("default", ["reload"]);
--- js/main.js
+++ js/main.js
@@ -1,87 +1,94 @@
-var app ={};
+var app = {};
 
 app.testimonials = testimonialsArray;
 
-app.buildTestimonial = function displayTestimonial(testimonialObj){
-	
+app.buildTestimonial = function displayTestimonial(testimonialObj) {
 	var testimonial = testimonialObj,
 		paragraphs,
 		linkAndName,
 		template;
-	
+
 	linkAndName = createLinkName(testimonial.link, testimonial.name); // returns link and name in string format
-		
+
 	paragraphs = createParagraph(testimonial.testimony); // returns testimony in string format surrounded by <p> tags
 
-	template = '<div class="yelp">' + paragraphs + linkAndName + '</div>';
-	
+	template = '<div class="yelp">' + paragraphs + linkAndName + "</div>";
+
 	// helper functions
-	function createLinkName(link, name){
-		var linkName = '<p><strong><a href="' + link + '" target="_blank">' + name +  '<i class="fa fa-angle-right"></i></a></strong></p>';
-		
+	function createLinkName(link, name) {
+		var linkName =
+			'<p><strong><a href="' +
+			link +
+			'" target="_blank">' +
+			name +
+			'<i class="fa fa-angle-right"></i></a></strong></p>';
+
 		return linkName;
 	}
 
-	function createParagraph(testimony){
-		var str = '';
-		for(var i = 0; i < testimony.length; i++){
-			str += '<p>' + testimony[i] + '</p>';
+	function createParagraph(testimony) {
+		var str = "";
+		for (var i = 0; i < testimony.length; i++) {
+			str += "<p>" + testimony[i] + "</p>";
 		}
 
 		return str;
 	}
-	
+
 	return template;
 };
 
-app.displayTestimonials = function displayTestimonials(testimonialsArray, amount){
-
+app.displayTestimonials = function displayTestimonials(
+	testimonialsArray,
+	amount
+) {
 	var arr = testimonialsArray.slice(), // creating a new reference rather than editing the original array
-		amountToDisplay = (amount) ? arr.length - amount : arr.length,
+		amountToDisplay = amount ? arr.length - amount : arr.length,
 		count = 0;
-		
+
 	shuffle(arr);
 
 	// Have a reversed loop because when I remove an item from the arr it gets re-indexed
-	for(var i = arr.length - 1; i >= amountToDisplay; i--){
-		
+	for (var i = arr.length - 1; i >= amountToDisplay; i--) {
 		var template = app.buildTestimonial(arr[i]);
 		var objIndex = arr.indexOf(arr[i]);
-		
+
 		//$('#testimonials').before('#viewRemainingTestimonials').append(template);
-		
-		$(template).insertBefore('#viewRemainingTestimonials');
-		
+
+		$(template).insertBefore("#viewRemainingTestimonials");
+
 		arr.splice(objIndex, 1);
 		count++;
 	}
 
-	if(arr.length > 0){
+	if (arr.length > 0) {
 		return arr; // when finished displaying first three testimonials, it will return a new arr with only the testimonials that are ready to display on See more Testimonials button.
 	}
-
 };
 
-app.generateImages = function(){
-
+app.generateImages = function() {
 	// This method is generating 15 random images from the images/min folder and displaying them.
 	// the loop is picking numbers at random and checking the number to make sure no numbers are being duplicated.
-	
+
 	var amountOfImagesInFolder = 40; // amount of images in the folder
 	var amountOfImagesToShow = 15; // how many images to show
-	var folderPath = '/images/min/'; // folder all images are in
-	var str = ''; // going to be the string that will get dumped into the page
-	
-	var fileNameConvention = 'image-';
+	var folderPath = "/images/min/"; // folder all images are in
+	var str = ""; // going to be the string that will get dumped into the page
+
+	var fileNameConvention = "image-";
 	var usedNumbers = [];
-	
-	for(var i = 1; i < amountOfImagesToShow; i++){
-		var randomImageNumber = Math.floor(Math.random() * amountOfImagesInFolder);
-		
-		var image = folderPath + fileNameConvention + randomImageNumber + '.jpg';
-		var tag = '<li><img src="http://testapainting.com' + image + '" /></li>';
-			
-		if($.inArray(randomImageNumber, usedNumbers) === -1){ // if the number is NOT in the array
+
+	for (var i = 1; i < amountOfImagesToShow; i++) {
+		var randomImageNumber = Math.floor(
+			Math.random() * amountOfImagesInFolder
+		);
+
+		var image =
+			folderPath + fileNameConvention + randomImageNumber + ".jpg";
+		var tag = '<li><img src="' + image + '" /></li>';
+
+		if ($.inArray(randomImageNumber, usedNumbers) === -1) {
+			// if the number is NOT in the array
 			usedNumbers.push(randomImageNumber); // add number to array
 			str += tag; // add <img> tag to string
 		} else {
@@ -89,18 +96,23 @@ app.generateImages = function(){
 			i--;
 		}
 	}
-	
-	return str;
-}; 
 
-$('#banner').append(app.generateImages()).bxSlider({
-	mode: 'fade',
-	speed: 350,
-	pager: false,
-	auto: true,
-	autoStart: true,
-	autoHover: true
-}); 
+	return str;
+};
+
+$("#banner")
+	.append(app.generateImages())
+	.bxSlider({
+		mode: "fade",
+		speed: 350,
+		pager: false,
+		auto: true,
+		autoStart: true,
+		autoHover: true,
+		adaptiveHeight: true,
+	});
 
 var lastTestimonials = app.displayTestimonials(app.testimonials, 3);
 
+// set current copyright year
+document.getElementById("currentYear").innerText = new Date().getFullYear();
